c8765b9caa41abb735b5c73e54fd1b2f0b812724
shaj13 <hajsanad@gmail.com>
2021-03-24 07:18:44 -0700

    add test wal disk

--- internal/disk/disk.go
+++ internal/disk/disk.go
@@ -6,11 +6,11 @@ import (
 
 	"go.etcd.io/etcd/pkg/v3/fileutil"
 	"go.etcd.io/etcd/raft/v3/raftpb"
-	"go.etcd.io/etcd/server/v3/etcdserver/api/snap"
 	"go.etcd.io/etcd/server/v3/wal"
 	"go.etcd.io/etcd/server/v3/wal/walpb"
-	"go.uber.org/zap"
 )
+
+// TODO: need to connect gc to disk and creat Purge method.
 
 type Disk struct {
 	wal     *wal.WAL
@@ -41,30 +41,27 @@ func (d *Disk) SaveEntries(st raftpb.HardState, entries []raftpb.Entry) error {
 
 // Boot return wal metadata, hard-state, entries, and newest snapshot,
 // Otherwise, it create new wal from given metadata alongside snapshots dir.
-func (d *Disk) Boot(meta []byte) ([]byte, raftpb.HardState, []raftpb.Entry, *raftpb.Snapshot, error) {
-	// TODO: get zap looger from cfg
-	fail := func(err error) ([]byte, raftpb.HardState, []raftpb.Entry, *raftpb.Snapshot, error) {
+func (d *Disk) Boot(meta []byte) ([]byte, raftpb.HardState, []raftpb.Entry, *SnapshotFile, error) {
+	fail := func(err error) ([]byte, raftpb.HardState, []raftpb.Entry, *SnapshotFile, error) {
 		return []byte{}, raftpb.HardState{}, []raftpb.Entry{}, nil, err
 	}
 
 	if !fileutil.Exist(d.snapdir) {
-		if err := os.Mkdir(d.snapdir, 0750); err != nil {
+		if err := os.MkdirAll(d.snapdir, 0750); err != nil {
 			return fail(
 				fmt.Errorf("raft: failed to create snapshot dir, Err: %s", err),
 			)
 		}
 	}
 
-	tempsnap := snap.New(zap.NewExample(), d.snapdir)
-
 	if !wal.Exist(d.waldir) {
-		if err := os.Mkdir(d.waldir, 0750); err != nil {
+		if err := os.MkdirAll(d.waldir, 0750); err != nil {
 			return fail(
 				fmt.Errorf("raft: failed to create WAL dir, Err: %s", err),
 			)
 		}
 
-		w, err := wal.Create(zap.NewExample(), d.waldir, meta)
+		w, err := wal.Create(nil, d.waldir, meta)
 		if err != nil {
 			return fail(
 				fmt.Errorf("raft: failed to create WAL dir, Err: %s", err),
@@ -75,7 +72,7 @@ func (d *Disk) Boot(meta []byte) ([]byte, raftpb.HardState, []raftpb.Entry, *raf
 		return meta, raftpb.HardState{}, []raftpb.Entry{}, nil, nil
 	}
 
-	walSnaps, err := wal.ValidSnapshotEntries(zap.NewExample(), d.waldir)
+	walSnaps, err := wal.ValidSnapshotEntries(nil, d.waldir)
 
 	if err != nil {
 		return fail(
@@ -83,9 +80,10 @@ func (d *Disk) Boot(meta []byte) ([]byte, raftpb.HardState, []raftpb.Entry, *raf
 		)
 	}
 
-	snapshot, err := tempsnap.LoadNewestAvailable(walSnaps)
-	if err == snap.ErrNoSnapshot {
-		snapshot = new(raftpb.Snapshot)
+	sf, err := ReadNewestAvailableSnapshot(d.snapdir, walSnaps)
+	if err == ErrNoSnapshot {
+		sf = new(SnapshotFile)
+		sf.Snap = new(raftpb.Snapshot)
 	} else if err != nil {
 		return fail(
 			fmt.Errorf("raft: failed to load newest snapshot, Err: %s", err),
@@ -93,11 +91,11 @@ func (d *Disk) Boot(meta []byte) ([]byte, raftpb.HardState, []raftpb.Entry, *raf
 	}
 
 	walsnap := walpb.Snapshot{
-		Index: snapshot.Metadata.Index,
-		Term:  snapshot.Metadata.Term,
+		Index: sf.Snap.Metadata.Index,
+		Term:  sf.Snap.Metadata.Term,
 	}
 
-	w, err := wal.Open(zap.NewExample(), d.waldir, walsnap)
+	w, err := wal.Open(nil, d.waldir, walsnap)
 	if err != nil {
 		return fail(
 			fmt.Errorf("raft: failed to open WAL, Err: %s", err),
@@ -112,9 +110,13 @@ func (d *Disk) Boot(meta []byte) ([]byte, raftpb.HardState, []raftpb.Entry, *raf
 	}
 
 	d.wal = w
-	return meta, st, ents, snapshot, nil
+	return meta, st, ents, sf, nil
 }
 
 func (d *Disk) Exist() bool {
 	return wal.Exist(d.waldir)
+}
+
+func (d *Disk) Close() {
+	d.wal.Close()
 }
---
+++ internal/disk/disk_test.go
@@ -0,0 +1,110 @@
+package disk
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"go.etcd.io/etcd/pkg/v3/fileutil"
+	"go.etcd.io/etcd/raft/v3/raftpb"
+	"go.etcd.io/etcd/server/v3/wal"
+	"go.etcd.io/etcd/server/v3/wal/walpb"
+)
+
+func TestDiskWalInteraction(t *testing.T) {
+	dir := createTestDir("wal", t)
+	defer os.RemoveAll(dir)
+
+	sf, _ := snapshotTestFile()
+	hs := raftpb.HardState{
+		Term:   sf.Snap.Metadata.Term,
+		Commit: sf.Snap.Metadata.Index,
+	}
+
+	// create wal and append data using disk objec.
+	w, _ := wal.Create(nil, dir, nil)
+	disk := Disk{
+		wal: w,
+	}
+
+	err := disk.SaveSnapshot(*sf.Snap)
+	assert.NoError(t, err)
+
+	err = disk.SaveEntries(hs, []raftpb.Entry{})
+	assert.NoError(t, err)
+
+	// clsoe disk
+	disk.Close()
+
+	// open wal for read and check data against waht saved.
+	snaps, _ := wal.ValidSnapshotEntries(nil, dir)
+	w, _ = wal.OpenForRead(nil, dir, walpb.Snapshot{})
+	_, gotHs, _, _ := w.ReadAll()
+
+	assert.Equal(t, sf.Snap.Metadata.Index, snaps[1].Index)
+	assert.Equal(t, sf.Snap.Metadata.Term, snaps[1].Term)
+	assert.Equal(t, gotHs, hs)
+
+}
+
+func TestDiskBootMkdir(t *testing.T) {
+	temp := filepath.Join(os.TempDir(), "/test_disk_boot")
+	defer os.RemoveAll(temp)
+	d := new(Disk)
+	d.snapdir = ""
+	d.waldir = ""
+
+	_, _, _, _, err := d.Boot(nil)
+	assert.Contains(t, err.Error(), "create snapshot dir")
+
+	d.snapdir = os.TempDir()
+	_, _, _, _, err = d.Boot(nil)
+	assert.Contains(t, err.Error(), "create WAL dir")
+
+	// it now should create dir
+	d.snapdir = temp
+	d.waldir = temp
+	_, _, _, _, err = d.Boot(nil)
+	assert.NoError(t, err)
+	assert.True(t, fileutil.Exist(temp))
+}
+
+func TestDiskBoot(t *testing.T) {
+	temp := filepath.Join(os.TempDir(), "/test_disk_boot")
+	defer os.RemoveAll(temp)
+
+	d := new(Disk)
+	d.snapdir = temp
+	d.waldir = temp
+
+	t.Run("it return error when wal locked", func(t *testing.T) {
+		defer d.Close()
+		os.RemoveAll(temp)
+
+		_, _, _, _, err := d.Boot(nil)
+		assert.NoError(t, err)
+
+		_, _, _, _, err = d.Boot(nil)
+		assert.Contains(t, err.Error(), "file already locked")
+	})
+
+	t.Run("it open the existing wal", func(t *testing.T) {
+		defer d.Close()
+		os.RemoveAll(temp)
+		meta := []byte("wal metadata")
+
+		_, _, _, _, err := d.Boot(meta)
+		assert.NoError(t, err)
+		d.Close()
+
+		got, _, _, _, err := d.Boot(nil)
+		assert.NoError(t, err)
+		assert.Equal(t, meta, got)
+	})
+}
+
+func TestDiskExist(t *testing.T) {
+	d := new(Disk)
+	assert.False(t, d.Exist())
+}
