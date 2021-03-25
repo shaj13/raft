be977b13d6ab68643fe2ea74691f11f216bb5608
shaj13 <hajsanad@gmail.com>
2021-03-25 12:17:56 -0700

    fix: add membership pool

---
+++ internal/membership/pool.go
@@ -0,0 +1,137 @@
+package membership
+
+import (
+	"context"
+	"fmt"
+	"math/rand"
+	"sync"
+	"time"
+
+	"github.com/shaj13/raftkit/api"
+	"github.com/shaj13/raftkit/internal/log"
+)
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func New(ctx context.Context, rep reporter, cfg config, dial Dial) *Pool {
+	return &Pool{
+		factory: newFactory(ctx, rep, cfg, dial),
+		membs:   make(map[uint64]Member),
+	}
+}
+
+type Pool struct {
+	factory *factory
+	mu      sync.Mutex // protects membs
+	membs   map[uint64]Member
+}
+
+func (p *Pool) NextID() uint64 {
+	var id uint64
+	for {
+		id = uint64(rand.Int63()) + 1
+		if _, ok := p.Get(id); !ok {
+			break
+		}
+	}
+	return id
+}
+
+func (p *Pool) Members() []Member {
+	membs := []Member{}
+	p.mu.Lock()
+	for _, m := range p.membs {
+		membs = append(membs, m)
+	}
+	p.mu.Unlock()
+	return membs
+}
+
+func (p *Pool) Get(id uint64) (Member, bool) {
+	p.mu.Lock()
+	m, ok := p.membs[id]
+	p.mu.Unlock()
+	return m, ok
+}
+
+func (p *Pool) Add(m api.Member) error {
+	mem, ok := p.Get(m.ID)
+	if ok && mem.Address() == m.Address {
+		// member already exist
+		return nil
+	}
+
+	if ok {
+		return p.Update(m)
+	}
+
+	p.mu.Lock()
+	defer p.mu.Unlock()
+
+	mem, ok, err := p.factory.From(m)
+	if !ok || err != nil {
+		return err
+	}
+
+	p.membs[m.ID] = mem
+	return nil
+}
+
+func (p *Pool) Update(m api.Member) error {
+	mem, ok := p.Get(m.ID)
+	if ok && mem.Address() == m.Address {
+		// member already exist
+		return nil
+	}
+
+	if !ok {
+		return p.Add(m)
+	}
+
+	return mem.Update(m.Address)
+}
+
+func (p *Pool) Remove(m api.Member) error {
+	mem, ok := p.Get(m.ID)
+	if !ok {
+		return fmt.Errorf("raft/membership: member %x not found", m.ID)
+	}
+
+	if mem.Type() == m.Type {
+		return nil
+	}
+
+	p.mu.Lock()
+	defer p.mu.Unlock()
+
+	mem.Close()
+
+	mem, ok, err := p.factory.Cast(mem, m.Type)
+	if !ok || err != nil {
+		return err
+	}
+
+	p.membs[m.ID] = mem
+	return nil
+}
+
+func (p *Pool) Snapshot() []api.Member {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+	membs := []api.Member{}
+	for _, mem := range p.membs {
+		m := p.factory.To(mem)
+		membs = append(membs, m)
+	}
+	return membs
+}
+
+func (p *Pool) Restore(pool api.Pool) {
+	for _, m := range pool.Members {
+		if err := p.Add(m); err != nil {
+			log.Errorf("raft/membership: Failed to add member %x, Err: %s", m.ID, err)
+		}
+	}
+}
---
+++ internal/membership/pool_test.go
@@ -0,0 +1,98 @@
+package membership
+
+import (
+	"context"
+	"testing"
+
+	"github.com/shaj13/raftkit/api"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+func TestPoolNextID(t *testing.T) {
+	p := newTestPool()
+	rec := make(map[uint64]struct{})
+
+	for i := 0; i < 10; i++ {
+		id := p.NextID()
+		_, ok := rec[id]
+		if ok {
+			t.Error("pool generate same id more than once")
+		}
+		p.membs[id] = nil
+		rec[id] = struct{}{}
+	}
+}
+
+func TestPoolUpdate(t *testing.T) {
+	m := &api.Member{ID: 1, Type: api.SelfMember}
+	p := newTestPool()
+	p.Add(*m)
+	m.Address = "5050"
+	p.Update(*m)
+	mem, _ := p.Get(m.ID)
+	assert.Equal(t, m.Address, mem.Address())
+}
+
+func TestPoolRemoveErr(t *testing.T) {
+	p := newTestPool()
+	err := p.Remove(api.Member{})
+	assert.Contains(t, err.Error(), "not found")
+}
+
+func TestPoolRestore(t *testing.T) {
+	p := newTestPool()
+	ids := make(map[uint64]struct{})
+	pool := &api.Pool{
+		Members: []api.Member{},
+	}
+
+	for i := 0; i < 5; i++ {
+		m := api.Member{ID: uint64(i), Type: api.SelfMember}
+		ids[uint64(i)] = struct{}{}
+		pool.Members = append(pool.Members, m)
+	}
+
+	p.Restore(*pool)
+
+	for id := range ids {
+		_, ok := p.Get(id)
+		if !ok {
+			t.Errorf("pool have'nt add member %x during the restore", id)
+		}
+	}
+}
+
+func TestPoolSnapshot(t *testing.T) {
+	p := newTestPool()
+	p.Add(api.Member{ID: p.NextID(), Type: api.SelfMember})
+	membs := p.Snapshot()
+	assert.Equal(t, len(p.Members()), len(membs))
+}
+
+func TestPoolRemove(t *testing.T) {
+	m := &api.Member{ID: 1, Type: api.SelfMember}
+
+	r := &mockReporter{mock.Mock{}}
+	r.On("ReportShutdown", m.ID).Return()
+
+	p := newTestPool()
+	p.factory.rep = r
+	p.Add(*m)
+
+	// run twice to check removing
+	// a removed member does not return error
+	for i := 0; i < 2; i++ {
+		m.Type = api.RemovedMember
+		err := p.Remove(*m)
+		mem, _ := p.Get(m.ID)
+		assert.NoError(t, err)
+		assert.Equal(t, api.RemovedMember, mem.Type())
+	}
+
+}
+
+func newTestPool() *Pool {
+	p := New(context.Background(), nil, testConfig, nil)
+	return p
+}
