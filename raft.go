e9a572fdd709591a687d030666f3ba06d5ef133c
shaj13 <hajsanad@gmail.com>
2021-11-02 21:32:15 +0200

    split raft/controller to standalone file

--- raft.go
+++ raft.go
@@ -1,15 +1,11 @@
 package raft
 
 import (
-	"context"
-
 	"github.com/shaj13/raftkit/internal/daemon"
 	"github.com/shaj13/raftkit/internal/membership"
-	"github.com/shaj13/raftkit/internal/raftpb"
 	"github.com/shaj13/raftkit/internal/storage/disk"
 	itransport "github.com/shaj13/raftkit/internal/transport"
 	"github.com/shaj13/raftkit/transport"
-	etcdraftpb "go.etcd.io/etcd/raft/v3/raftpb"
 )
 
 func New(proto transport.Proto, opts ...Option) *Node {
@@ -35,36 +31,4 @@ func New(proto transport.Proto, opts ...Option) *Node {
 	cfg.controller.(*controller).pool = cfg.pool
 
 	return node
-}
-
-type controller struct {
-	node   *Node
-	daemon daemon.Daemon
-	pool   membership.Pool
-}
-
-func (c *controller) Join(ctx context.Context, m *raftpb.Member) (uint64, []raftpb.Member, error) {
-	var err error
-
-	if mm, _ := c.node.GetMemebr(m.ID); mm == nil {
-		err = c.node.AddMember(ctx, m)
-	} else {
-		err = c.node.UpdateMember(ctx, m)
-	}
-
-	if err != nil {
-		return 0, []raftpb.Member{}, err
-	}
-
-	memb, _ := c.node.GetMemebr(m.ID)
-	pool := c.pool.Snapshot()
-	return memb.ID(), pool, nil
-}
-
-func (c *controller) Push(ctx context.Context, m etcdraftpb.Message) error {
-	return c.daemon.Push(m)
-}
-
-func (c *controller) PromoteMember(ctx context.Context, m raftpb.Member) error {
-	return c.node.promoteMember(ctx, m.ID, true)
 }
