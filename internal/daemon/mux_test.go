75805f87a6b84e96b3206507d6a9a8e3f3d73b9e
shaj13 <hajsanad@gmail.com>
2021-11-17 12:10:10 +0200

    fix: add test mux push

--- internal/daemon/mux_test.go
+++ internal/daemon/mux_test.go
@@ -1,7 +1,9 @@
 package daemon
 
 import (
+	"context"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 )
@@ -49,4 +51,37 @@ func TestMuxOp(t *testing.T) {
 		require.Equal(t, testGroupID, op.gid)
 		require.Equal(t, tt.ot, op.ot)
 	}
+}
+
+func TestMuxPush(t *testing.T) {
+	mux := NewMux().(*mux)
+	op := new(operation)
+	ctx, cancel := context.WithCancel(context.TODO())
+	cancel()
+
+	// it return err when ctx done.
+	err := mux.push(ctx, op)
+	require.Equal(t, context.Canceled, err)
+
+	// it return err when mux done.
+	close(mux.done)
+	err = mux.push(context.TODO(), op)
+	require.Equal(t, ErrStopped, err)
+
+	mux.done = make(chan struct{})
+	mux.operationc = make(chan *operation, 10)
+
+	// it return err when ctx done and op pushed.
+	ctx, cancel = context.WithTimeout(context.TODO(), time.Millisecond*50)
+	defer cancel()
+	err = mux.push(ctx, op)
+	require.Equal(t, context.DeadlineExceeded, err)
+
+	// it return err when mux done and op pushed.
+	go func() {
+		time.Sleep(time.Millisecond * 50)
+		close(mux.done)
+	}()
+	err = mux.push(context.TODO(), op)
+	require.Equal(t, ErrStopped, err)
 }
